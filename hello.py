e2752068d5b5e18251eb2a5fb3e113ecc523564c
Ellison Chan <ellison.chan@gmail.com>
2020-09-12 14:39:00 +0000

    full hello

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print ("Hello Ellison!")
+from utils import *
+
+def main()
+def main():
+    job_id = int(os.environ['JOB_ID'])
+    DATA_DIR = '/datasets/cifar10-data'
+
+    f = open(DATA_DIR+"/output.txt")
+    f.write("Ellison {} was here!".format(job_id))
+    f.close()
+
+if __name__ == '__main__':
+    main()
+
